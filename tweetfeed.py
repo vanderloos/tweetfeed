79c890f30d7aa5cefe55a5151ab07421c73565a0
Lexa <lexamailo@gmail.com>
2013-07-19 11:51:55 +0300

    Wrapped class again

--- tweetfeed.py
+++ tweetfeed.py
@@ -15,40 +15,55 @@ def printRow(string):
     a = a.encode('utf8', 'replace')
     return a
 
+class MyFeed:
+	def __init__(self, consumer_key='', consumer_secret='', access_token_key='77175560-', access_token_secret='', xml_out_file = "pyrss2gen.xml"):
+		self.consumer_key = consumer_key
+		self.consumer_secret = consumer_secret
+		self.access_token_key = access_token_key
+		self.access_token_secret = access_token_secret
+		self.statuses_file = ""
+		self.title = "Vanderloos PyRSS2Gen feed"
+		self.link = "http://twitter.com/van_der_loos"
+		self.description = "The latest tweets from vanderloos' friends"
+		self.xml_out_file = xml_out_file
 
-api = twitter.Api(consumer_key='XXXXXXXXX',
-                      consumer_secret='XXXXXXXXXXXX',
-                      access_token_key='XXXXXXXXXXXXXX',
-                      access_token_secret='XXXXXXXXXXXX')
+	def get_feed(self):
+		api = twitter.Api(self.consumer_key, self.consumer_secret, self.access_token_key, self.access_token_secret)
 
-friends = api.GetFriends()
-rss = PyRSS2Gen.RSS2(
-    title = "Vanderloos PyRSS2Gen feed",
-    link = "http://twitter.com/van_der_loos",
-    description = "The latest tweets from vanderloos' friends",
+		friends = api.GetFriends()
+		rss = PyRSS2Gen.RSS2(self.title, self.link, self.description,
+			lastBuildDate = datetime.datetime.now(), items = [])
+		if self.statuses_file:
+			twitter_statuses = open(self.statuses_file,'w')
+			twitter_statuses.write('')
+			twitter_statuses.close()
+			twitter_statuses = open(self.statuses_file,'a')
+		statuses_sum = []
 
-    lastBuildDate = datetime.datetime.now(),
+		for f in friends:
+			statuses_sum += api.GetUserTimeline(user_id = f.id, since_id = 357931428379500544)
+		statuses_sum.sort(key=lambda tweet: tweet.created_at_in_seconds, reverse=True)
 
-    items = [])
-statuses_sum = []
+		for tweet in statuses_sum:
+			item = PyRSS2Gen.RSSItem(title = printRow(tweet.user.name + ': ' + tweet.text))
+			tweet_url = r'https://twitter.com/' + tweet.user.screen_name + r'/status/' + str(tweet.id)
+			if tweet.in_reply_to_status_id:
+				item.description = r'In reply to: https://twitter.com/' + tweet.in_reply_to_screen_name + r'/status/' + str(tweet.in_reply_to_status_id)
+			if tweet.urls:
+				item.link = tweet.urls[0].expanded_url
 
-for f in friends[:5]:
-	statuses_sum += api.GetUserTimeline(f.id)
-statuses_sum.sort(key=lambda tweet: tweet.created_at_in_seconds, reverse=True)
-
-for tweet in statuses_sum:
-	item = PyRSS2Gen.RSSItem(title = printRow(tweet.user.name + ': ' + tweet.text))
-	tweet_url = r'https://twitter.com/' + tweet.user.screen_name + r'/status/' + str(tweet.id)
-	if tweet.in_reply_to_status_id:
-		item.description = r'In reply to: https://twitter.com/' + tweet.in_reply_to_screen_name + r'/status/' + str(tweet.in_reply_to_status_id)
-	if tweet.urls:
-		item.link = tweet.urls[0].expanded_url
-
-	item.guid = PyRSS2Gen.Guid(tweet_url)
-	item.pubDate = tweet.created_at
-	item.author = printRow(tweet.user.name)
-         
-	rss.items.append(item)	
-
-twitter_statuses.close()
-rss.write_xml(open("pyrss2gen.xml", "w"))
+			item.guid = PyRSS2Gen.Guid(tweet_url)
+			item.pubDate = tweet.created_at
+			item.author = printRow(tweet.user.name)
+				 
+			rss.items.append(item)	
+			if self.statuses_file:
+				twitter_statuses.write(printRow(tweet.user.name)+'\n')
+				twitter_statuses.write(printRow(tweet.text)+'\n')
+	
+		rss.write_xml(open(self.xml_out_file, "w"))
+		if self.statuses_file:
+			twitter_statuses.close()
+def get_feed():
+	a = MyFeed()
+	a.get_feed()
